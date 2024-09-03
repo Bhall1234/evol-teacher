4f1f1340dafd5e1cd44a1fc0558354635c24288a
CringeWalker <64558379+Bhall1234@users.noreply.github.com>
2024-09-03 22:54:13 +0100

    `Refactor statistical tests code for improved readability and consistency`

--- My_Work/statistical_tests/mann-whitney.py
+++ My_Work/statistical_tests/mann-whitney.py
@@ -4,7 +4,7 @@ import scipy.stats as stats
 group_a_interactive_scores = [4, 4] 
 group_b_interactive_scores = [2, 2]  
 
-# Perform the test for comparing interactive system scores between Group A and Group B
+# test for comparing interactive system scores between Group A and Group B
 stat, p_value = stats.mannwhitneyu(group_a_interactive_scores, group_b_interactive_scores)
 print('Mann-Whitney U Test (Interactive System): Stat=%.3f, p=%.3f' % (stat, p_value))
 
@@ -13,7 +13,7 @@ print('Mann-Whitney U Test (Interactive System): Stat=%.3f, p=%.3f' % (stat, p_v
 group_a_non_interactive_scores = [4, 3]  
 group_b_non_interactive_scores = [4, 4] 
 
-# Perform the test for comparing non-interactive system scores between Group A and Group B
+# test for comparing non-interactive system scores between Group A and Group B
 stat_non, p_value_non = stats.mannwhitneyu(group_a_non_interactive_scores, group_b_non_interactive_scores)
 print('Mann-Whitney U Test (Non-Interactive System): Stat=%.3f, p=%.3f' % (stat_non, p_value_non))
 
